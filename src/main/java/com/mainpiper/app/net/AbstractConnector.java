896b2e9ac3057d2960196dca80228df958907422
Christian Kula <christian.kula.ck@gmail.com>
2016-08-29 09:36:31 +0200

    Réduction de la réptition du code dans AbstractConnector

--- src/main/java/com/mainpiper/app/net/AbstractConnector.java
+++ src/main/java/com/mainpiper/app/net/AbstractConnector.java
@@ -1,17 +1,17 @@
 package com.mainpiper.app.net;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.mainpiper.app.model.Chapter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.parser.Parser;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mainpiper.app.model.Chapter;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +20,13 @@ import lombok.Setter;
 @Setter
 
 public abstract class AbstractConnector {
+	private final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11";
+
+	private final String HEADER_ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+	private final String HEADER_ACCEPT_CHARSET = "ISO-8859-1,utf-8;q=0.7,*;q=0.3";
+	private final String HEADER_LANGUAGE = "en-US,en;q=0.8";
+	private final String HEADER_KEEP_ALIVE = "keep-alive";
+
 	protected final static int MAX_TRIES = 3;
 	protected final static int TIME_OUT_IN_MILLIS = 5000;
 	protected final String WEBSITE_GENERIC_URL;
@@ -29,33 +36,31 @@ public abstract class AbstractConnector {
 		// TODO get the mangas url from enum
 	}
 
-	// TODO variable final
-	public static Connection jsoupConnectionHTML(String url) {
-		return Jsoup.connect(url)
-				.header("User-Agent",
-						"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
-				.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-				.header("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3")
-				.header("Accept-Language", "en-US,en;q=0.8")
-				.header("keep-alive", "keep-alive")
-				.timeout(TIME_OUT_IN_MILLIS)
-				.ignoreContentType(true)
-				.parser(Parser.htmlParser());
+	public Connection jsoupConnectionHTML(String url) {
+		return createJsoupConnection(url, true);
 	}
 
-	public static Connection jsoupConnectionRSS(String url) {
-		return Jsoup.connect(url)
-				.header("User-Agent",
-						"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
-				.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-				.header("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3")
-				.header("Accept-Language", "en-US,en;q=0.8")
-				.header("keep-alive", "keep-alive")
-				.timeout(TIME_OUT_IN_MILLIS)
-				.parser(Parser.xmlParser());
-
+	public Connection jsoupConnectionRSS(String url) {
+		return createJsoupConnection(url, false);
 	}
 	// FIXME do single header
+
+	private Connection createJsoupConnection(String url, boolean htmlParser) {
+		Connection jsoupConnection = Jsoup.connect(url)
+				.userAgent(USER_AGENT)
+				.header("Accept", HEADER_ACCEPT)
+				.header("Accept-Charset", HEADER_ACCEPT_CHARSET)
+				.header("Accept-language", HEADER_LANGUAGE)
+				.header("keep-alive", HEADER_KEEP_ALIVE)
+				.timeout(TIME_OUT_IN_MILLIS);
+		if (htmlParser) {
+			jsoupConnection.parser(Parser.htmlParser());
+		} else {
+			jsoupConnection.parser(Parser.xmlParser());
+		}
+
+		return jsoupConnection;
+	}
 
 	public String getSiteUrl() {
 		return WEBSITE_GENERIC_URL;
