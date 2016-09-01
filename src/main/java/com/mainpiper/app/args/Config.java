d9527524bc1e1052d305043d7ce6da040ace5b8a
Bérenger OUADI <bouadi@mediametrie.fr>
2016-09-01 15:17:13 +0200

    Add Error Managment (2/2) + Add config (1/2)

--- src/main/java/com/mainpiper/app/args/Config.java
+++ src/main/java/com/mainpiper/app/args/Config.java
@@ -1,81 +1,26 @@
 package com.mainpiper.app.args;
 
+import org.apache.commons.cli.CommandLine;
+
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Getter
 public class Config {
 
-    private String Default_S3_Output;
-    private String S3_Alarms_Path;
+    private String scriptName;
 
-    private String dbURL;
-    private String MasterUsername;
-    private String MasterUserPassword;
+    private CommandLine cli;
 
-    private String sourceTableName;
-    private String csoTableName;
-    private String rpaTableName;
+    public void extendConfig(CommandLine cliContent) {
+        cli = cliContent;
 
-    private CliOptions cli;
-    private String output;
-
-    // public void extendConfig(CliOptions cli) {
-    // this.cli = cli;
-    // String outputTemp = cli.getOutput();
-    // if (outputTemp == null) {
-    // output = Default_S3_Output;
-    // } else {
-    // output = outputTemp;
-    // }
-    // }
-    //
-    // public void displayDebug() {
-    // log.debug("input : {}", args.getInput());
-    // log.debug("output : {}", output);
-    // log.debug("type : {}", args.getType());
-    // log.debug("special Values : {}", args.getSpecialValues());
-    // log.debug("Default Output : {}", Default_S3_Output);
-    // log.debug("Alarms file : {}", S3_Alarms_Path);
-    // log.debug("Redshift Url : {}", dbURL);
-    // log.debug("Source Table : {}", sourceTableName);
-    // log.debug("Rpa Table : {}", rpaTableName);
-    // log.debug("Cso Table : {}", csoTableName);
-    // }
-
-    public String getDefaultS3Output() {
-        return Default_S3_Output;
     }
 
-    public String getDefaultAlarmsPath() {
-        return S3_Alarms_Path;
-    }
+    public void displayDebug() {
+        log.debug("input : {}", scriptName);
 
-    public String getRedShiftDBUrl() {
-        return dbURL;
-    }
-
-    public String getRedshiftMasterUsername() {
-        return MasterUsername;
-    }
-
-    public String getRedshiftMasterPassword() {
-        return MasterUserPassword;
-    }
-
-    public String getSourceTableName() {
-        return sourceTableName;
-    }
-
-    public String getRpaTableName() {
-        return rpaTableName;
-    }
-
-    public String getCsoTableName() {
-        return csoTableName;
-    }
-
-    public CliOptions getCliOptions() {
-        return cli;
     }
 
 }
--- src/main/java/com/mainpiper/app/main/Main.java
+++ src/main/java/com/mainpiper/app/main/Main.java
@@ -4,57 +4,70 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.ParseException;
 
 import com.mainpiper.app.args.CliOptions;
+import com.mainpiper.app.exceptions.TerminateBatchException;
 import com.mainpiper.app.service.Service;
 
-// @Slf4j
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
-	public static final String APP_VERSION = "1.0";
+    public static final String APP_VERSION = "1.0";
 
-	public static void main(String[] args) {
-		CommandLine commandLine = null;
-		Service service = null;
-		String mangaName = null;
+    public static void main(String[] args) {
+        CommandLine commandLine = null;
+        Service service = null;
+        String mangaName = null;
 
-		try {
-			CommandLineParser cliParser = new DefaultParser();
-			commandLine = cliParser.parse(CliOptions.getInstance(), args);
-		} catch (Exception e) {
-			printUsageAndExit("Wrong usage");
-		}
+        try {
+            CommandLineParser cliParser = new DefaultParser();
+            commandLine = cliParser.parse(CliOptions.getInstance(), args);
 
-		if (args.length > 0) {
-			mangaName = args[0];
+            if (args.length > 0) {
+                mangaName = args[0];
 
-			service = new Service(mangaName, null);
+                // elle est pas nul elle vaut la valeur cli de source qui par default est nul!
+                service = new Service(mangaName, null);
 
-			if (commandLine.hasOption(CliOptions.OPT_HELP)) {
-				printUsageAndExit("Help");
-			} else if (commandLine.hasOption(CliOptions.OPT_VERSION)) {
-				printVersion();
-			}
+                if (commandLine.hasOption(CliOptions.OPT_HELP)) {
+                    printUsageAndExit("Help");
+                } else if (commandLine.hasOption(CliOptions.OPT_VERSION)) {
+                    printVersion();
+                }
 
-			if (commandLine.hasOption(CliOptions.OPT_VOLUME) && commandLine.hasOption(CliOptions.OPT_CHAPTER)) {
-				service.downloadChapters(commandLine.getOptionValue(CliOptions.OPT_CHAPTER));
-			} else {
-				service.downloadManga();
-			}
-		} else {
-			printUsageAndExit("Wrong usage");
-		}
-	}
+                if (commandLine.hasOption(CliOptions.OPT_VOLUME) && commandLine.hasOption(CliOptions.OPT_CHAPTER)) {
+                    service.downloadChapters(commandLine.getOptionValue(CliOptions.OPT_CHAPTER));
+                } else {
+                    service.downloadManga();
+                }
+            } else {
+                printUsageAndExit("Wrong usage");
+            }
+        } catch (ParseException e) {
+            log.debug("Erreur in the Cli parsing : ", e);
+            printUsageAndExit("Wrong usage");
+        } catch (TerminateBatchException ex) {
+            log.error("Batch failure with code: {}\n", ex.getExitCode(), ex);
+            System.exit(ex.getExitCode());
+        } catch (Exception ex) {
+            log.error("Unknown exception: ", ex);
+            System.exit(TerminateBatchException.EXIT_CODE_UNKNOWN);
+        }
 
-	private static void printVersion() {
-		System.out.println("Manga downloader v" + Main.APP_VERSION);
-		System.exit(0);
-	}
+    }
 
-	private static void printUsageAndExit(String errorMsg) {
-		System.out.println(errorMsg);
-		HelpFormatter hf = new HelpFormatter();
-		hf.setWidth(768);
-		hf.printHelp("manga_downloader <mangas name>", CliOptions.getInstance(), true);
-		System.exit(1);
-	}
+    private static void printVersion() {
+        System.out.println("Manga downloader v" + Main.APP_VERSION);
+        System.exit(0);
+    }
+
+    private static void printUsageAndExit(String errorMsg) {
+        System.out.println(errorMsg);
+        HelpFormatter hf = new HelpFormatter();
+        hf.setWidth(768);
+        hf.printHelp("manga_downloader <mangas name>", CliOptions.getInstance(), true);
+        System.exit(1);
+    }
 }
